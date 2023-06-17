6ffc761bc8905a871286906887a06af32f604042
Germano Ribeiro <germanno.verde@gmail.com>
2023-06-17 20:21:09 -0300

    JOIPJOP

--- Verificador de Idade/vericIdade.js
+++ Verificador de Idade/vericIdade.js
@@ -4,7 +4,7 @@ function verificar(){
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     if(fano.value.length == 0 || Number(fano.value) > ano){
-    window.alert("ERRO! Verifique os dados e tente novamente!")
+    window.alert("ERRO! Verifique os dados e pente novamente!")
 }else {
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
